b4ca2d1e388512e443bd75206b1884313e44d072
Lincoln.chawora <Lincoln.chawora@digital.hmrc.gov.uk>
2017-10-16 14:26:09 +0100

    character colides with wall, start of inventory

--- MyGame/Assets/_Scripts/Walking.cs
+++ MyGame/Assets/_Scripts/Walking.cs
@@ -6,12 +6,12 @@ public class Walking : MonoBehaviour {
 
 	private Animator anim;
 
-//	public float speed = 1f;
-//	public float rotationSpeed = 100.0f;
+	public float speed = 1f;
+	public float rotationSpeed = 100.0f;
 
-	public float vert;
-	public float hori;
-	public float sprint;
+//	public float vert;
+//	public float hori;
+//	public float sprint;
 	
 	// Use this for initialization
 	void Start () {
@@ -21,51 +21,47 @@ public class Walking : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		vert = Input.GetAxis("Vertical");
-		hori = Input.GetAxis("Horizontal");
-		Sprinting();
-		
+//		vert = Input.GetAxis("Vertical");
+//		hori = Input.GetAxis("Horizontal");
+//		Sprinting();
+
+		if(Input.GetButtonDown("Jump")) {
+			anim.SetTrigger("isJumping");
+
+		}
 
 
+		float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
 
-//		if(Input.GetButtonDown("Jump")) {
-//			anim.SetTrigger("isJumping");
-//
-//		}
-//
-//
-//		float translation = Input.GetAxis("Vertical") * speed;
-//        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-//        translation *= Time.deltaTime;
-//        rotation *= Time.deltaTime;
-//        transform.Translate(0, 0, translation);
-//        transform.Rotate(0, rotation, 0);
-//
-//        if(translation != 0) {
-//        	anim.SetBool("isRunning", true);
-//        	anim.SetBool("isIdle", false);
-//
-//        } else {
-//			anim.SetBool("isRunning", false);
-//			anim.SetBool("isIdle", true);
-//        }
+        if(translation != 0) {
+        	anim.SetBool("isRunning", true);
+        	anim.SetBool("isIdle", false);
+
+        } else {
+			anim.SetBool("isRunning", false);
+			anim.SetBool("isIdle", true);
+        }
 
 
 	}
 
-	void FixedUpdate () {
-		anim.SetFloat("walk", vert);
-		anim.SetFloat("Turn", hori);
-		anim.SetFloat("run", sprint);
-
-	}
-
-	void Sprinting() {
-
-		if(Input.GetButton("Fire1")){
-			sprint = 0.2f;
-		} else {
-			sprint = 0.0f;
-		}	
-	}
+//	void FixedUpdate () {
+//		anim.SetFloat("walk", vert);
+//		anim.SetFloat("Turn", hori);
+//		anim.SetFloat("run", sprint);
+//
+//	}
+//
+//	void Sprinting() { 
+//		if(Input.GetButton("Fire1")){
+//			sprint = 0.2f;
+//		} else {
+//			sprint = 0.0f;
+//		}	
+//	}
 }
