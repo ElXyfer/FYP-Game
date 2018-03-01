f0794285f7759278115fc11f90aa51eda3410ef9
Lincoln.chawora <Lincoln.chawora@digital.hmrc.gov.uk>
2018-03-01 00:27:06 +0000

    book animation workinng - 4 hours

--- MyGame/Assets/_Scripts/PlayerController.cs
+++ MyGame/Assets/_Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private Animator anim;
+	Animator anim;
+    BookScript bookController;
 
     public float speed;
 	public float sprintSpeed;
@@ -15,61 +16,75 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
+        bookController = GetComponent<BookScript>();
 	}
 
 
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
 
-		if(Input.GetKeyDown("space")) {
-			anim.SetBool("isAttacking", true);
-		}
-
-        if (Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown("space"))
         {
-            anim.SetBool("bookisOpen", true);
+            anim.SetBool("isAttacking", true);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
         }
 
-		float translation = Input.GetAxis("Vertical") * speed;
+        if(Input.GetKeyDown(KeyCode.B)) {
+            anim.SetBool("bookisOpen", true);
+            anim.SetBool("isAttacking", false);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+        }
+
+        float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
 
-        if(translation != 0) {
-			isMoving = true;
-			//if(isMoving == true){
-				speed = 1f;
-        	anim.SetBool("isWalking", true);
-            anim.SetBool("bookisOpen", false);
-            anim.SetBool("isRunning", false);
+        if (translation != 0)
+        {
+            isMoving = true;
+            if (isMoving == true)
+            {
+                speed = 1f;
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isAttacking", false);
 
-        	if(Input.GetKey(KeyCode.Z)){
-        		anim.SetBool("isRunning", true);
-        		speed = sprintSpeed;
-        	}
-        	//}
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    anim.SetBool("isRunning", true);
+                    speed = sprintSpeed;
+                }
+            }
 
-        } else {
-            anim.SetBool("isWalking", false);
         }
-	}
-
-	public void EnemyIsDead(){
-        if(HitDetection.EnemyHealth <= 0){
-			anim.SetBool("isIdle", true);
-		}
-	}
-
-    void OpenBook() {
-        
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", true);
+        }
 
     }
 
+    public void BookTakeOutComplete() {
+        anim.enabled = false;
+        bookController.LookAtBook();
+    }
 
-
-
-
+    public void EnemyIsDead()
+    {
+        if (HitDetection.EnemyHealth <= 0)
+        {
+            anim.SetBool("isIdle", true);
+        }
+    }
 
 }
